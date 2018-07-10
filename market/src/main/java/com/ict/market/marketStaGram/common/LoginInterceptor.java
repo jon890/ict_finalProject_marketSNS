df71e81f00140b226aeac816b3f64aae7d184e06
jon890 <36876250+jon890@users.noreply.github.com>
2018-07-10 17:32:42 +0900

    미비수정및인터셉터보완중..

---
+++ market/src/main/java/com/ict/market/marketStaGram/common/LoginInterceptor.java
@@ -0,0 +1,21 @@
+package com.ict.market.marketStaGram.common;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		if(session.getAttribute("id") == null) {
+			response.sendRedirect("/market/login.main");
+			return false;
+		}
+		return true;
+	}	
+}
